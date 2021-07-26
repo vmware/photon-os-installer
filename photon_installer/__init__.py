198c831161fab32b598491a6e9e9a7f9039d274f
Shreenidhi Shedi <sshedi@vmware.com>
2021-07-26 15:53:04 +0530

    Add version to installer with git commit hash info
    This info will help to check for installer updates in Photon builder.
    
    Change-Id: I24bd6d9039578d9e959c3e0a622d0ff8c9b0e08c
    Signed-off-by: Shreenidhi Shedi <sshedi@vmware.com>

--- photon_installer/__init__.py
+++ photon_installer/__init__.py
@@ -1,10 +1,16 @@
-#/*
-# * Copyright © 2020 VMware, Inc.
-# * SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-only
-# */
-from os.path import dirname, basename, isfile, join
-import glob
+#
+# Copyright © 2020-2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-only
+#
+
 import sys
+import glob
+import pkg_resources
+from os.path import dirname, basename, isfile, join
+
+
 modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
 sys.path.append(dirname(__file__))
+
+__version__ = pkg_resources.get_distribution(__name__).version
--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
-#/*
-# * Copyright © 2020 VMware, Inc.
-# * SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-only
-# */
+#
+# Copyright © 2020-2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-only
+#
+
+from version import get_installer_version
 from setuptools import setup, find_packages
 
+
 setup(name='photon-installer',
-      version='4.0',
       description='Installer code for photon',
       packages=find_packages(include=['photon_installer', 'photon_installer.modules']),
       include_package_data=True,
+      version='1.0+'+get_installer_version(),
       author_email='gpiyush@vmware.com')
