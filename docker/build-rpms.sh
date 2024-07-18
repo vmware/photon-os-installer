1ce87fd1d6ba3addca41ecd9976661572c1c1b93
Oliver Kurth <okurth@gmail.com>
2024-07-18 22:02:30 +0000

    revert 4aae514, causing build breakage with TDNF_OPTIONS set

--- docker/build-rpms.sh
+++ docker/build-rpms.sh
@@ -6,13 +6,11 @@ PACKAGE=$1
 SPEC=${PACKAGE}.spec
 VERSION=$(rpmspec -q --srpm --queryformat "[%{VERSION}\n]" ${SPEC})
 FULLNAME=${PACKAGE}-${VERSION}
-IFS=$'\n' BUILD_REQUIRES=( $(rpmspec -q --buildrequires ${SPEC} 2>/dev/null) )
+BUILD_REQUIRES=$(rpmspec -q --buildrequires ${SPEC})
 TARBALL=$(rpmspec -q --srpm --queryformat "[%{SOURCE}\n]" ${SPEC})
 ARCH=$(uname -m)
 RPM_BUILD_DIR="/usr/src/photon"
 DIST=.ph5
-
-echo BuildRequires: "${BUILD_REQUIRES[@]}"
 
 # if checked out as a submodule .git is a file, pointing to the parent
 # if pwd is mounted the parent is not accessible
@@ -28,11 +26,10 @@ else
     tar zcf ${TARBALL} --exclude=${TARBALL} --transform "s,^./,${FULLNAME}/," .
 fi
 
-tdnf install -y ${TDNF_OPTIONS} "${BUILD_REQUIRES[@]}"
+tdnf install -y ${TDNF_OPTIONS} ${BUILD_REQUIRES}
 
 mkdir -p ${RPM_BUILD_DIR}
 mkdir -p ${RPM_BUILD_DIR}/{SOURCES,BUILD,RPMS,SRPMS}
 mv ${TARBALL} ${RPM_BUILD_DIR}/SOURCES/
 
 rpmbuild --nodeps -D "dist ${DIST}" -D "_topdir ${RPM_BUILD_DIR}" -ba ${SPEC}
-createrepo_c ${RPM_BUILD_DIR}/RPMS
